876332a650383b9526edc54d0dd7758adb8c92ec
David Ing <daverinoe@gmail.com>
2021-02-09 11:06:40 +0100

    1D Diffusion added
    Added 1D diffusion, though I had to turn my viscosity down two orders of magnitude compared to the tutorial for the solution to converge.

--- _main.jl
+++ _main.jl
@@ -22,11 +22,17 @@ plotly()
 
 # First, let's build up from 1D, and go from there.
 
+# Discretising the diffusion involves a second-order derivative. Taylor-expand with foward and backward difference
+# schemes, then combine to cancel terms. Neglect O(x^4) terms.
+
+# When done properly, gives d2u/dx2 = (ui+1 - 2ui + ui-1)/Delx2 + O(Delx^2)
+
 nx = 41
 dx = 2 / (nx - 1)
-nt = 25 # Number of timesteps
-dt = 0.025 # dt is the amount of time each step, i.e. delta t
-c = 1 # Wavespeed
+nt = 20 # Number of timesteps
+nu = 0.01 # Viscosity
+sigma = 0.2 # Sigma is randomly defined later, then we're told to ignore it for now. K. Probably some kind of discretised stepping rate.
+dt = sigma * dx * dx / ν # dt is the amount of time each step, i.e. delta t
 
 # Set up initial conditions. We want 1 everywhere, except where there are twos (0.5 ≤ x ≤ 1)
 u = ones(1, nx)
@@ -37,8 +43,8 @@ plot(u')
 # For every element in u, we need to perform the discretization
 for n in 1:nt
     local u_n = copy(u)
-    for i in 2:nx
-        u[i] = u_n[i] - u_n[i] * dt / dx * (u_n[i] - u_n[i-1]) # Non-linear term added here (c -> u_n[i]).
+    for i in 2:nx-1
+        u[i] = u_n[i] + nu * dt / (dx * dx) * (u_n[i+1] - 2 * u_n[i] + u_n[i-1]) # Discretised diffusion added here. It's linear, but based on viscosity.
     end
 end
 
